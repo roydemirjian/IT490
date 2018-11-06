25178ed50fdad43512371e56c7cc99645fe2ef6e
RoyDemirjian <roykebab@gmail.com>
2018-11-06 15:24:53 -0500

    "Api data pull test working"

--- apiTest.php
+++ apiTest.php
@@ -1,14 +1,43 @@
 #!/usr/bin/php
 <?php
 
-$ch = curl_init("https://jsonplaceholder.typicode.com/posts/1");
-$fp = fopen("api_data.txt", "w");
+echo "Number: 1 For Movie Search or 2 for TV Show\n";
 
-curl_setopt($ch, CURLOPT_FILE, $fp);
-curl_setopt($ch, CURLOPT_HEADER, 0);
+$number = readline("Enter one number from above: ");
+if ($number == "1") {
+	$input = readline("Movie: ");
+	$moviename = preg_replace('/\s+/', '+',$input);
 
-curl_exec($ch);
-curl_close($ch);
-fclose($fp);
+
+	$ch = curl_init("https://api.themoviedb.org/3/search/movie?api_key=a99025c572bede9218ee420b5c9f4cc4&language=en-US&query=" . $moviename . "&page=1&include_adult=false");
+	$fp = fopen("apidata.txt", "w+");
+
+	curl_setopt($ch, CURLOPT_FILE, $fp);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+
+	curl_exec($ch);
+	curl_close($ch);
+	fclose($fp);
+
+}else{
+
+	$input2 = readline("TV Show: ");
+	$showname = preg_replace('/\s+/', '+',$input2);
+
+
+	$ch = curl_init("https://api.themoviedb.org/3/search/tv?api_key=a99025c572bede9218ee420b5c9f4cc4&language=en-US&query=" . $showname . "&page=1");
+	$fp = fopen("apidata.txt", "w+");
+
+	curl_setopt($ch, CURLOPT_FILE, $fp);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+
+	curl_exec($ch);
+	curl_close($ch);
+	fclose($fp);
+
+}
 
 ?>
+
+
+
