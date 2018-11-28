3a8a276cc953d5ee61e7513a8bac797f5492e232
RoyDemirjian <roykebab@gmail.com>
2018-11-27 21:20:03 -0500

    "Working on incremental version numbers"

--- deployment/backuptest.sh
+++ deployment/backuptest.sh
@@ -6,10 +6,12 @@ backup_files="/home/roydem/database"
 # Where to backup to.
 dest="/home/roydem/backups"
 
+# version_num= $1
+# version_n=$1
+
 # Create archive filename.
 current_time=$(date +%m-%d-%Y_%H-%M-%S)
-hostname=$(hostname -s)
-archive_file="database-backup-$hostname-$current_time.tgz"
+archive_file="database-backup-$current_time.tgz"
 
 # Print start status message.
 echo "Backing up $backup_files to $dest/$archive_file"
@@ -27,6 +29,9 @@ echo
 
 # SCP the tar that was just made to the deploy server 
 scp /home/roydem/backups/* roydem@192.168.1.184:/home/roydem/database/scp
+
+#delete local copy once tar has reached server
+rm -r /home/roydem/backups/*
 
 # Long listing of files in $dest to check file sizes.
 ls -lh $dest
